3f39965e4ef9e67feeee8b3bd367761330f0ac2b
boui <blissfulboui@naver.com>
2024-09-01 23:58:18 +0900

    0901_23:58

--- function.c
+++ function.c
@@ -1,7 +1,7 @@
 #include "function.h"
 #include "card.h"
 
-int GetRandomNumber(int max, int min) // 랜덤값 반환 함수
+int GetRandomNumber(int min, int max) // 랜덤값 반환 함수
 {
     return rand() % (max - min + 1) + min;
     // min~max에서 랜덤값 1개 반환
@@ -10,7 +10,7 @@ THE_CARD *ResetCard(void) // 새 게임 시 카드 기본값 초기화 함수
 {
     THE_CARD *gamecard = (THE_CARD *)malloc(sizeof(THE_CARD));
     // 카드 기본값 동적 할당
-    if(gamecard == NULL)
+    if (gamecard == NULL)
     {
         puts("Program Error \n");
         return NULL;
@@ -18,13 +18,16 @@ THE_CARD *ResetCard(void) // 새 게임 시 카드 기본값 초기화 함수
     for (int suits = 0; suits < 4; suits++)
     {
         for (int number = 0; number < 10; number++)
+        {
             gamecard->card[suits][number] = number + 1;
+        }
     }
-    return gamecard; // 동적 할당된 메모리 반환
+    return gamecard; // 동적 할당된 메모리 주소 반환
 }
 char *GetSuits(int suits_num) // 카드 문양 전달 함수
 {
-    char *suits;
+    char *suits=(char*)malloc(4*sizeof(char));
+    // UTF-8로 인코딩된 문자로 \0 포함 4바이트 필요
     switch (suits_num)
     {
     case 1:
@@ -52,7 +55,8 @@ void GetCard(THE_CARD *gamecard, int *sequence) // 카드 출력 함수
 {
     int random_number;
     int random_suits;
-    char *first_or_second = (*sequence) == FIRST ? "First Card" : "Second Card";
+    char *suits=NULL;   // 카드문양 저장
+    char *first_or_second = *sequence == FIRST ? "First Card" : "Second Card";
     // sequence 값이 0이면 첫번째 or 1이면 두번째
     do
     {
@@ -67,47 +71,53 @@ void GetCard(THE_CARD *gamecard, int *sequence) // 카드 출력 함수
         gamecard->second_card = gamecard->card[random_suits][random_number];
     *sequence = !(*sequence); // 첫번째 or 두번째 변경
 
-    printf("%s [ %s%d ] \n", first_or_second, GetSuits(random_suits), gamecard->card[random_suits][random_number]);
+    ClearScreen(); // 화면 지우기
+    suits=GetSuits(random_suits);
+    printf("%s [ %s%d ] \n", first_or_second, suits, gamecard->card[random_suits][random_number]);
+    free(suits);    // 문양 출력이 끝나면 메모리 해제
     gamecard->card[random_suits][random_number] = 0; // 한번 나온 카드값을 0으로 초기화
     // 한번 나온 카드는 다시 나오지 않음
 }
-bool OddEvenResult(THE_CARD *gamecard)  // 홀수 짝수 결과 반환 함수
+bool OddEvenResult(THE_CARD *gamecard) // 홀수 짝수 결과 반환 함수
 {
-    bool result= (gamecard->first_card+gamecard->second_card)%2 == 0 ? true : false;
+    bool result = (gamecard->first_card + gamecard->second_card) % 2 == 0 ? true : false;
     // 첫번째 카드와 두번째 카드를 더한 값이 짝수=true 홀수=false
     return result;
 }
 void GameResult(THE_CARD *gamecard, GAME_BET_RESULT *bet_results)
-{   
+{
+    ResultAllocateMemory(bet_results);
     // 홀수 짝수를 맞췄을 때
-    if(OddEvenResult(gamecard) == bet_results->odd_even_choice)
+    if (OddEvenResult(gamecard) == bet_results->odd_even_choice)
     {
         puts("[ Succeed ]");
-        strcpy(bet_results->game_results,"Succeed");
+        strcpy(bet_results->game_results[bet_results->num_results], "Succeed");
+        bet_results->num_results += 1;
     }
 
     // 틀렸을 때
     else
     {
         puts("[ Fail ]");
-        strcpy(bet_results->game_results,"Fail");
+        strcpy(bet_results->game_results[bet_results->num_results], "Fail");
+        bet_results->num_results += 1;
     }
 }
 void ChooseOddEven(GAME_BET_RESULT *bet_results)
 {
     int choice;
-    ShowOddEvenBetItem();   // 홀 짝 베팅 항목 출력
-    scanf("%d",&choice);
+    ShowOddEvenBetItem(); // 홀 짝 베팅 항목 출력
+    scanf("%d", &choice);
     // 홀수 선택 시:false 짝수 선택 시:true
-    bet_results->odd_even_choice=choice == 1 ? false : true;
+    bet_results->odd_even_choice = choice == 1 ? false : true;
 }
-void ResultAllocateMemory(GAME_BET_RESULT *bet_results)  // 게임 결과 저장 메모리 할당, 추가
+void ResultAllocateMemory(GAME_BET_RESULT *bet_results) // 게임 결과 저장 메모리 할당, 추가
 {
-    if(bet_results->num_results+1 == bet_results->size_memory_results)
+    if (bet_results->num_results + 1 == bet_results->size_memory_results)
     {
         // 결과 저장 멤버의 메모리가 부족할 시 5 증가
-        bet_results->game_results=(char(*)[RESULT_SIZE])
-        realloc(bet_results->game_results,sizeof(bet_results->game_results)*(bet_results->size_memory_results+5));
-        bet_results->size_memory_results += 5;  // 늘린 메모리크기 저장
+        bet_results->game_results = (char(*)[RESULT_SIZE])
+            realloc(bet_results->game_results, sizeof(bet_results->game_results) * (bet_results->size_memory_results + 5));
+        bet_results->size_memory_results += 5; // 늘린 메모리크기 저장
     }
 }
--- show.c
+++ show.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ClearScreen()  // 콘솔 화면 지우는 함수
+void ClearScreen() // 콘솔 화면 지우는 함수
 {
-    system("cls");  // windows에서 콘솔 화면을 지우는 명령어
+    system("cls"); // windows에서 콘솔 화면을 지우는 명령어
 }
-void ShowListGames(void)    // 게임 목록 출력 함수
+void ShowListGames(void) // 게임 목록 출력 함수
 {
     ClearScreen();
-    puts("\n---List Of Games--- \n" 
-         "1. ODD-EVEN GAME \n"
-         "2. LOW-HIGH GAME \n"
-         "3. SUITS GUESSING GAME \n"
-         "4. CARD SIZE MATCH GAME \n"
-         "5. EXIT \n\n"
+    printf("\n---List Of Games--- \n"
+           "1. ODD-EVEN GAME \n"
+           "2. LOW-HIGH GAME \n"
+           "3. SUITS GUESSING GAME \n"
+           "4. CARD SIZE MATCH GAME \n"
+           "5. EXIT \n\n"
 
-         "Select The Game: ");
+           "Select The Game: ");
 }
-void ShowOddEvenGameMenu(void)  // 홀/짝 게임 메뉴 출력 함수
+void ShowOddEvenGameMenu(void) // 홀/짝 게임 메뉴 출력 함수
 {
     ClearScreen();
-    puts("\n[ODD-EVEN GAME]\n"
-         "1. New Game \n"
-         "2. Continue \n"
-         "3. Result \n"
-         "4. EXIT \n\n"
+    printf("\n[ODD-EVEN GAME]\n"
+           "1. New Game \n"
+           "2. Continue \n"
+           "3. Result \n"
+           "4. EXIT \n\n"
 
-         "Select The Menu: ");
+           "Select The Menu: ");
 }
-void ShowOddEvenGamePlay(void)  // 홀 짝 게임 플레이 메뉴 출력 함수
+void ShowOddEvenGamePlay(void) // 홀 짝 게임 플레이 메뉴 출력 함수
 {
     ClearScreen();
-    puts("\n[GAME PLAY]\n"
-         "1. Game Start \n"
-         "2. End \n\n"
-         
-         "Select: ");
+    printf("\n[GAME PLAY]\n"
+           "1. Game Start \n"
+           "2. End \n\n"
+
+           "Select: ");
 }
-void ShowOddEvenBetItem(void)   // 홀수 짝수 베팅 항목 출력 함수
+void ShowOddEvenBetItem(void) // 홀수 짝수 베팅 항목 출력 함수
 {
-    puts("1. ODD \n"
-         "2. EVEN \n\n"
-         
-         "What Is Your Choice: ");
+    printf("1. ODD \n"
+           "2. EVEN \n\n"
+
+           "What Is Your Choice: ");
 }
